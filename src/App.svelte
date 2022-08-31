fdc59ab6ba2eb6d70c3aa9ee51b9d198ec874fbf
Pablo Tedesco <tedesco8@hotmail.com>
2022-08-30 21:40:33 -0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<div class="footer">
+	Footer Application in <img src="https://bluuweb.github.io/svelte-youtube/img/svelte.png" class="svelte-logo"/>
+</div>
+<style>
+	.footer{
+		background-color: #aaa;
+		font-size: 18px;
+		font-weight: bold;
+		text-align: center;
+		height:100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.svelte-logo{
+		height:70px;
+	}
+
+</style>
